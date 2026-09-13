ec9354bfc4d63fb03e7bba18bd8b57b02aaf8e22
Andrew Dodonov <nordhummer@gmail.com>
2026-09-13 21:44:50 +0300

    [test] add tests for main

--- src/test/java/app/MainTest.java
+++ src/test/java/app/MainTest.java
@@ -35,4 +35,36 @@ class MainTest {
         assertThrows(ValidationException.class, () -> MenuHandler.userInput(scanner));
     }
 
+    // хэппи вей для рандома. Если порядок ввода поменяется - тест сломается!
+    // в целом рандом тестить такое себе )
+    @Test
+    void userInput_withValidRandomFlow_returnsDataWithoutExceptions() {
+        Scanner scanner = new Scanner("1\n5\n3\n1\n");
+
+        MenuHandler.UserInput input = MenuHandler.userInput(scanner);
+
+        assertEquals(5, input.loaded().size());
+        assertEquals(BusField.MILEAGE, input.field());
+        assertInstanceOf(BubbleSortStrategy.class, input.strategy());
+    }
+
+    // Если порядок ввода поменяется - тест сломается!
+    @Test
+    void userInput_withInvalidAlgorithmChoice_throwsValidationException() {
+        Scanner scanner = new Scanner("1\n5\n3\n9\n");
+
+        assertThrows(ValidationException.class, () -> MenuHandler.userInput(scanner));
+    }
+
+    // Если порядок ввода поменяется - тест сломается!
+    @Test
+    void userInput_withInvalidSourceChoice_throwsValidationException() {
+        Scanner scanner = new Scanner("9\n");
+
+        assertThrows(ValidationException.class, () -> MenuHandler.userInput(scanner));
+    }
+
+
+
+
 }
